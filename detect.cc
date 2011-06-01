33b10f8d459df406d9232c46c7a1a9c872ba49e9
Alex Churchill <achur@stanford.edu>
2011-06-01 03:13:35 -0700

    whatever

--- detect.cc
+++ detect.cc
@@ -1,7 +1,25 @@
+// Switch to __LINUX_BUILD if you want it
+#define __BSD_BUILD
+
+#if defined(__BSD_BUILD)
+#define GETSYN(x) ((x->th_flags & TH_SYN) > 0 ? 1 : 0)
+#define GETACK(x) ((x->th_flags & TH_ACK) > 0 ? 1 : 0)
+#define GETRST(x) ((x->th_flags & TH_RST) > 0 ? 1 : 0)
+#define GETSRCPORT(x) (x->th_sport)
+#define GETDSTPORT(x) (x->th_dport)
+#define GETSEQ(x) (x->th_seq)
+#endif // defined(__BSD_BUILD)
+
+#if defined(__LINUX_BUILD)
+#define GETSYN(x) ((x->syn) > 0 ? 1 : 0)
+#define GETACK(x) ((x->ack) > 0 ? 1 : 0)
+#endif // defined(__LINUX_BUILD)
+
 #include <iostream>
-#include <stdio.h>
-#include <unistd.h>
 #include <pcap.h>
+#include <netinet/ip.h>
+#include <netinet/tcp.h>
+#include <arpa/inet.h>
 
 using namespace std;
 
@@ -34,7 +52,19 @@ void runDetection(pcap_t* pcap) {
       cout << "Error in reading a packet" << endl;
     }
     if(result == -2) break;
-    
+    struct ip* ip_hdr = (struct ip*) (packet_data+14);
+    if(ip_hdr->ip_p != 6) cout << "test" << endl; // If we don't have a TCP packet.
+    char src[80];
+    inet_ntop(ip_hdr->ip_v == 4 ? AF_INET : AF_INET6, &(ip_hdr->ip_src), src, 80);
+    char dst[80];
+    inet_ntop(ip_hdr->ip_v == 4 ? AF_INET : AF_INET6, &(ip_hdr->ip_dst), dst, 80);
+    struct tcphdr* tcp_hdr = (struct tcphdr*) (packet_data + 14 + ip_hdr->ip_hl * 4);
+    cout << "Source: " << src << "   Destination: " << dst << endl;
+    cout << "S_PORT: " << GETSRCPORT(tcp_hdr) << "   D_PORT: " << GETDSTPORT(tcp_hdr) << "  SEQ: " << GETSEQ(tcp_hdr) << endl;
+    if(GETSYN(tcp_hdr) > 0) cout << "SYN ";
+    if(GETACK(tcp_hdr) > 0) cout << "ACK ";
+    if(GETRST(tcp_hdr) > 0) cout << "RST";
+    cout << endl << endl;
   }
 }
 
