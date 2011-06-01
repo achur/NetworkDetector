4b856ae3e4877edef5657b350cb560a754502fce
Alex Churchill <achur@stanford.edu>
2011-05-31 23:38:06 -0700

    A tiny change

--- detect.cc
+++ detect.cc
@@ -3,13 +3,19 @@
 using namespace std;
 
 // Function Prototypes
+int runDetection(string filename);
 
 
-int main(int argc, char *argv[], char *env[])
-{
+int main(int argc, char *argv[], char *env[]) {
   if(argc != 2) {
     cout << "Usage: " + string(argv[0]) + " pcap_filename" << endl;
+    return -1;
   } else {
-    cout << argv[1] << endl;
+    return runDetection(argv[1]);
   }
 }
+
+int runDetection(string filename) {
+  cout << filename << endl;
+}
+
